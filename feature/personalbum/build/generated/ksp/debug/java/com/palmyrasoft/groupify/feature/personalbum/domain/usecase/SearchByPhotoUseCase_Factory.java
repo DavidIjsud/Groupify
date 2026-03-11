513789168111b60a24b3a889de609003e3a61946
David Garcia Romero <DavidIjsud@gmail.com>
2026-03-10 23:41:26 -0400

    ads

---
+++ feature/personalbum/build/generated/ksp/debug/java/com/palmyrasoft/groupify/feature/personalbum/domain/usecase/SearchByPhotoUseCase_Factory.java
@@ -0,0 +1,58 @@
+package com.palmyrasoft.groupify.feature.personalbum.domain.usecase;
+
+import com.palmyrasoft.groupify.feature.personalbum.domain.recognition.FaceEmbedder;
+import com.palmyrasoft.groupify.feature.personalbum.domain.repository.FaceIndexRepository;
+import com.palmyrasoft.groupify.feature.personalbum.domain.repository.PhotoRepository;
+import dagger.internal.DaggerGenerated;
+import dagger.internal.Factory;
+import dagger.internal.QualifierMetadata;
+import dagger.internal.ScopeMetadata;
+import javax.annotation.processing.Generated;
+import javax.inject.Provider;
+
+@ScopeMetadata
+@QualifierMetadata
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation"
+})
+public final class SearchByPhotoUseCase_Factory implements Factory<SearchByPhotoUseCase> {
+  private final Provider<FaceEmbedder> faceEmbedderProvider;
+
+  private final Provider<FaceIndexRepository> faceIndexRepositoryProvider;
+
+  private final Provider<PhotoRepository> photoRepositoryProvider;
+
+  public SearchByPhotoUseCase_Factory(Provider<FaceEmbedder> faceEmbedderProvider,
+      Provider<FaceIndexRepository> faceIndexRepositoryProvider,
+      Provider<PhotoRepository> photoRepositoryProvider) {
+    this.faceEmbedderProvider = faceEmbedderProvider;
+    this.faceIndexRepositoryProvider = faceIndexRepositoryProvider;
+    this.photoRepositoryProvider = photoRepositoryProvider;
+  }
+
+  @Override
+  public SearchByPhotoUseCase get() {
+    return newInstance(faceEmbedderProvider.get(), faceIndexRepositoryProvider.get(), photoRepositoryProvider.get());
+  }
+
+  public static SearchByPhotoUseCase_Factory create(Provider<FaceEmbedder> faceEmbedderProvider,
+      Provider<FaceIndexRepository> faceIndexRepositoryProvider,
+      Provider<PhotoRepository> photoRepositoryProvider) {
+    return new SearchByPhotoUseCase_Factory(faceEmbedderProvider, faceIndexRepositoryProvider, photoRepositoryProvider);
+  }
+
+  public static SearchByPhotoUseCase newInstance(FaceEmbedder faceEmbedder,
+      FaceIndexRepository faceIndexRepository, PhotoRepository photoRepository) {
+    return new SearchByPhotoUseCase(faceEmbedder, faceIndexRepository, photoRepository);
+  }
+}
---
+++ feature/personalbum/build/generated/ksp/debug/java/com/palmyrasoft/groupify/feature/personalbum/presentation/PersonAlbumViewModel_Factory.java
@@ -0,0 +1,78 @@
+package com.palmyrasoft.groupify.feature.personalbum.presentation;
+
+import androidx.work.WorkManager;
+import com.palmyrasoft.groupify.feature.personalbum.data.prefs.IndexingOnboardingPrefs;
+import com.palmyrasoft.groupify.feature.personalbum.domain.repository.FaceIndexRepository;
+import com.palmyrasoft.groupify.feature.personalbum.domain.usecase.BuildQueryFaceThumbnailsUseCase;
+import com.palmyrasoft.groupify.feature.personalbum.domain.usecase.DetectQueryFacesUseCase;
+import com.palmyrasoft.groupify.feature.personalbum.domain.usecase.SearchByPhotoUseCase;
+import dagger.internal.DaggerGenerated;
+import dagger.internal.Factory;
+import dagger.internal.QualifierMetadata;
+import dagger.internal.ScopeMetadata;
+import javax.annotation.processing.Generated;
+import javax.inject.Provider;
+
+@ScopeMetadata
+@QualifierMetadata
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation"
+})
+public final class PersonAlbumViewModel_Factory implements Factory<PersonAlbumViewModel> {
+  private final Provider<WorkManager> workManagerProvider;
+
+  private final Provider<SearchByPhotoUseCase> searchByPhotoUseCaseProvider;
+
+  private final Provider<DetectQueryFacesUseCase> detectQueryFacesUseCaseProvider;
+
+  private final Provider<BuildQueryFaceThumbnailsUseCase> buildQueryFaceThumbnailsUseCaseProvider;
+
+  private final Provider<FaceIndexRepository> faceIndexRepositoryProvider;
+
+  private final Provider<IndexingOnboardingPrefs> onboardingPrefsProvider;
+
+  public PersonAlbumViewModel_Factory(Provider<WorkManager> workManagerProvider,
+      Provider<SearchByPhotoUseCase> searchByPhotoUseCaseProvider,
+      Provider<DetectQueryFacesUseCase> detectQueryFacesUseCaseProvider,
+      Provider<BuildQueryFaceThumbnailsUseCase> buildQueryFaceThumbnailsUseCaseProvider,
+      Provider<FaceIndexRepository> faceIndexRepositoryProvider,
+      Provider<IndexingOnboardingPrefs> onboardingPrefsProvider) {
+    this.workManagerProvider = workManagerProvider;
+    this.searchByPhotoUseCaseProvider = searchByPhotoUseCaseProvider;
+    this.detectQueryFacesUseCaseProvider = detectQueryFacesUseCaseProvider;
+    this.buildQueryFaceThumbnailsUseCaseProvider = buildQueryFaceThumbnailsUseCaseProvider;
+    this.faceIndexRepositoryProvider = faceIndexRepositoryProvider;
+    this.onboardingPrefsProvider = onboardingPrefsProvider;
+  }
+
+  @Override
+  public PersonAlbumViewModel get() {
+    return newInstance(workManagerProvider.get(), searchByPhotoUseCaseProvider.get(), detectQueryFacesUseCaseProvider.get(), buildQueryFaceThumbnailsUseCaseProvider.get(), faceIndexRepositoryProvider.get(), onboardingPrefsProvider.get());
+  }
+
+  public static PersonAlbumViewModel_Factory create(Provider<WorkManager> workManagerProvider,
+      Provider<SearchByPhotoUseCase> searchByPhotoUseCaseProvider,
+      Provider<DetectQueryFacesUseCase> detectQueryFacesUseCaseProvider,
+      Provider<BuildQueryFaceThumbnailsUseCase> buildQueryFaceThumbnailsUseCaseProvider,
+      Provider<FaceIndexRepository> faceIndexRepositoryProvider,
+      Provider<IndexingOnboardingPrefs> onboardingPrefsProvider) {
+    return new PersonAlbumViewModel_Factory(workManagerProvider, searchByPhotoUseCaseProvider, detectQueryFacesUseCaseProvider, buildQueryFaceThumbnailsUseCaseProvider, faceIndexRepositoryProvider, onboardingPrefsProvider);
+  }
+
+  public static PersonAlbumViewModel newInstance(WorkManager workManager,
+      SearchByPhotoUseCase searchByPhotoUseCase, DetectQueryFacesUseCase detectQueryFacesUseCase,
+      BuildQueryFaceThumbnailsUseCase buildQueryFaceThumbnailsUseCase,
+      FaceIndexRepository faceIndexRepository, IndexingOnboardingPrefs onboardingPrefs) {
+    return new PersonAlbumViewModel(workManager, searchByPhotoUseCase, detectQueryFacesUseCase, buildQueryFaceThumbnailsUseCase, faceIndexRepository, onboardingPrefs);
+  }
+}
